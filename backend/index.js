3e515f49020bd143eac1afc1d11615d059485746
sakshamg567 <saksham060306@gmail.com>
2025-03-14 01:41:33 +0530

    Add CORS support to backend and update dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const analysisRouter = require("./routes/Analysis.router")
-
+const cors = require("cors")
 const app = express();
 app.use(express.json())
+app.use(cors({
+   origin: "https://disperse-chi.vercel.app/"
+}))
 app.use(express.urlencoded({extended: true}))
 app.get("/", (req, res) => {
    res.send("HELLO");
